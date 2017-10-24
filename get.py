4af6d7b3cd58511661ea190ac21f6bca3aeedece
noootown <i314i@yahoo.com.tw>
2017-10-24 08:18:47 +0800

    Initial commit

---
+++ get.py
@@ -0,0 +1,44 @@
+import subprocess
+import argparse
+import sys
+import re
+
+def parse_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--url', dest='url', help='download url', default='', type=str)
+  parser.add_argument('--cookie', dest='cookie', help='cookie', default='cookie', type=str)
+  parser.add_argument('--file', dest='filename', help='file', default='file', type=str)
+  args = parser.parse_args()
+
+  if len(sys.argv) == 1 or not args.url:
+    parser.print_help()
+    sys.exit(1)
+
+  return args
+
+if __name__ == '__main__':
+  args = parse_args()
+  gid = re.findall('^https?://drive.google.com/file/d/([^/]+)', args.url)[0]
+
+  def wget(url, filename = '', withCookie = False):
+    cmd = 'wget'
+    if withCookie:
+      cmd += ' -c'
+    cmd += ' --tries=3 --no-check-certificate --load-cookie %s --save-cookie %s %s' % (args.cookie, args.cookie, url)
+    if filename:
+      cmd += ' -O %s' % filename
+    subprocess.call(cmd.split(' '))
+
+  wget('https://docs.google.com/uc?id=%s&export=download' % gid, args.filename)
+
+  try:
+    with open(args.filename, 'r') as file:
+      href = re.findall('href="(\/uc\?export=download[^"]+)', file.read())
+
+      if href:
+        url = 'https://docs.google.com%s' % href[0].replace('&amp;', '&')
+        wget(url, args.filename, withCookie = True)
+  except:
+    pass
+
+  print('Finish downloading %s' % args.filename)
